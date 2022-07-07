3c9155f52b5581f14d408ef9fb6273fd57f15a6a
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-07-07 14:29:36 -0300

    feat: adiciona as configurações do footer da página

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
+import { AiOutlineTrademarkCircle } from "react-icons/ai";
 
 export default function Footer () {
     return (
         <Container>
-            Footer
+            SlugStore
+            <AiOutlineTrademarkCircle />
         </Container>
     )
 };
@@ -11,11 +13,16 @@ export default function Footer () {
 const Container = styled.div`
     width: 100vw;
     height: 10vh;
-    background-color: var(--amarelo);
+    background-color: var(--cor-cinza-escuro);
+    color: var(--branco);
     display: flex;
     justify-content: center;
     align-items: center;
     position: fixed;
     bottom: 0;
     left: 0;
+
+    svg {
+        color: var(--branco);
+    }
 `
