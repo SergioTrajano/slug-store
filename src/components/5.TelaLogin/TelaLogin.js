08ff932a7d949b6ebde0c7848b9a574659b9790d
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-07-07 12:35:38 -0300

    feat: adicionado elemento da rota /sign-in

--- src/components/5.TelaLogin/TelaLogin.js
+++ src/components/5.TelaLogin/TelaLogin.js
@@ -1,7 +1,157 @@
-
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { ThreeDots } from 'react-loader-spinner';
+import axios from 'axios';
+import UserContext from '../../UserContext';
 
 export default function TelaLogin () {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const { setToken, setNome, rotaAnterior, desabilitarClick, setDesabilitarClick } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    function submeter(e) {
+        e.preventDefault();
+        setDesabilitarClick(true);
+        const dadosLogin = {
+            headers: {
+                email,
+                senha,
+            }
+        }
+        const promise = axios.get('http://localhost:5001', dadosLogin);
+        promise.then((response) => {
+            setToken(response.data.token);
+            setNome(response.data.name);
+            navigate(`/${rotaAnterior}`);
+        });
+        promise.catch(() => {
+            alert('Dados inválidos!');
+            setDesabilitarClick(false);
+            setEmail('');
+            setSenha('');
+        });
+    }
+
+    const butaoConteudo = !desabilitarClick ? 'Fazer Login' : <ThreeDots color='white' />;
+
     return (
-        <div>TelaLogin</div>
-    )
-};
+        <Container>
+            <p>
+                Seja muito bem vindo(a)!
+            </p>
+            <form onSubmit={submeter}>
+                <input 
+                    placeholder='E-mail'
+                    type={'email'}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    disabled={desabilitarClick}
+                    required
+                />
+                <input 
+                    placeholder='Senha'
+                    type={'password'}
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                    disabled={desabilitarClick}
+                    required
+                />
+                <button disabled={desabilitarClick} type={'submit'}>
+                    {butaoConteudo}
+                </button>
+            </form>
+            <Link to={'/sign-up'}>
+                Não tem cadastro ainda? Cadastre-se agora!
+            </Link>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    width: 100vw;
+    min-height: 80vh;
+    position: absolute;
+    top: 10vh;
+    left: 0;
+    background-color: var(--branco);
+    font-family: 'Inter', sans-serif;
+    color: var(--preto);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+
+    p {
+        font-size: 4.79vh;
+        line-height: 5.24vh;
+        color: var(--preto);
+        font-weight: bold;
+        margin-top: 7vh;
+        margin-bottom: 4.49vh;
+        align-self: center;
+        width: 60vw;
+        text-align: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        input {
+            width: 86.93vw;
+            height: 8.69vh;
+            background-color: var(--branco);
+            color: var(--preto);
+            font-size: 2.99vh;
+            line-height: 3.52vh;
+            padding-left: 4vw;
+            margin-bottom: 2.24vh;
+            border-radius: 5px;
+            border: 1px solid var(--cinza-claro);
+
+            &::placeholder {
+                color: var(--cinza-claro);
+            }
+
+            &:disabled {
+                opacity: 0.8;
+            }
+        }
+
+        button {
+            width: 86.93vw;
+            height: 6.89vh;
+            text-align: center;
+            border: none;
+            background-color: var(--roxo);
+            color: var(--branco);
+            font-size: 2.99vh;
+            line-height: 3.52vh;
+            font-weight: bold;
+            border-radius: 5px;
+            margin-top: 3vh;
+
+            &:disabled {
+                opacity: 0.8;
+            }
+        }
+    }
+
+    a {
+        text-decoration: none;
+        font-size: 2.24vh;
+        line-height: 2.63vh;
+        font-weight: bold;
+        color: var(--preto);
+        margin-top: 5.39vh;
+        width: 60vw;
+        text-align: center;
+
+        &:disabled {
+            opacity: 0.8;
+        }
+    }
+`
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,52 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { useState } from "react";
+import UserContext from "../UserContext";
+import Header from "./shared/Header.js";
+import Footer from "./shared/Footer.js";
+import Modal from "./shared/Modal.js";
+import TelaHome from "./1.TelaHome/TelaHome.js";
+import TelaProdutos from "./2.TelaProdutos/TelaProdutos.js";
+import TelaProduto from "./3.TelaProduto/TelaProduto.js";
+import TelaCadastro from "./4.TelaCadastro/TelaCadastro.js";
+import TelaLogin from "./5.TelaLogin/TelaLogin.js";
+import TelaCarrinho from "./6.TelaCarrinho/TelaCarrinho.js";
 
 
-function App() {
+export default function App() {
+
+    const [token, setToken] = useState("");
+    const [nome, setNome] = useState("");
+    const [carrinho, setCarrinho] = useState("");
+    const [modalAberto, setModalAberto] = useState(false);
+    const [rotaAnterior, setRotaAnterior] = useState('');
+    const [desabilitarClick, setDesabilitarClick] = useState(false);
+    
+
+    const contextValue = { token, setToken,
+                            nome, setNome,
+                            carrinho, setCarrinho,
+                            modalAberto, setModalAberto,
+                            rotaAnterior, setRotaAnterior,
+                            desabilitarClick, setDesabilitarClick };
+
     return (
-        <>
-        </>
-    )
-}
 
-export default App;
+        <UserContext.Provider value={contextValue}>
+        <BrowserRouter>
+                <Header />
+                {modalAberto ? <Modal/> : null} 
+            <Routes>
+                <Route path="/" element={<TelaHome />} />
+                <Route path="/:type" element={<TelaProdutos />} />
+                <Route path="/:type/:id" element={<TelaProduto />} />
+                <Route path="/sign-up" element={<TelaCadastro />} />
+                <Route path="/sign-in" element={<TelaLogin />} />
+                <Route path="/cart" element={<TelaCarrinho />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+        </UserContext.Provider>
+
+    );
+}
